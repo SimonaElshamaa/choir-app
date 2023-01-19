4491dcbe42b5a0285b17d5ea1e936d3cc5bae638
SimonaSoliman34 <simona.soliman@ivasystem.fr>
2023-01-19 14:15:43 +0200

    remove comment

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,6 @@
 const apiPrefix = "/api";
 const APIURL = process.env.REACT_APP_API_URL;
 const ApiUrl = APIURL + apiPrefix;
-console.log(process.env);
 const requestTimeout = 30000;
 
 export default {
