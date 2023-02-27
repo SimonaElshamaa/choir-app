52ce9b31e586a4242693a802ee700f966d51b88e
SimonaSoliman34 <simona.soliman@ivasystem.fr>
2023-02-27 15:34:36 +0100

    display api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 const apiPrefix = "/api";
 const APIURL = process.env.REACT_APP_API_URL;
+console.log("API URL********",APIURL);
 const ApiUrl = APIURL + apiPrefix;
 const requestTimeout = 30000;
 
