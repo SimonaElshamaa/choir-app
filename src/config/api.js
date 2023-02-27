9cd879eca0846f9f656e7c1d555d6979f7333645
SimonaSoliman34 <simona.soliman@ivasystem.fr>
2023-02-27 15:54:30 +0100

    add ip as number

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 const apiPrefix = "/api";
 // const APIURL = process.env.REACT_APP_API_URL;
-const APIURL="https://localhost:3000"
+const APIURL="https://127.0.0.1:3000"
 console.log("API URL********",APIURL);
 const ApiUrl = APIURL + apiPrefix;
 const requestTimeout = 30000;
