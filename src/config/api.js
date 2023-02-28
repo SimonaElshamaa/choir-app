00dcba0dc30d9d4c43c278d72e4555df041f963f
SimonaSoliman34 <simona.soliman@ivasystem.fr>
2023-02-28 14:56:50 +0100

    change ip address

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 const apiPrefix = "/api";
 // const APIURL = process.env.REACT_APP_API_URL;
-const APIURL="https://165.227.168.9:3000"
+const APIURL="https://0.0.0.0:3000"
 console.log("API URL********",APIURL);
 const ApiUrl = APIURL + apiPrefix;
 const requestTimeout = 30000;
