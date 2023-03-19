3ee1f64ff75af067a2a15ed1ff8f1e5c25fc9a63
SimonaSoliman34 <simona.soliman@ivasystem.fr>
2023-03-19 19:31:11 +0100

    add new back end url

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 const apiPrefix = "/api";
 // const APIURL = process.env.REACT_APP_API_URL;
-// const APIURL="http://127.0.0.1:3000"
-const APIURL="https://www.dhattendance.com/api"
+const APIURL="https://api1qsdcawefv198.dhattendance.com"
 console.log("API URL********",APIURL);
-// const ApiUrl = APIURL + apiPrefix;
-const ApiUrl = APIURL ;
+const ApiUrl = APIURL + apiPrefix;
 const requestTimeout = 30000;
 
 export default {
