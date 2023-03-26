c03199719a82a6653ac3c473045db310c4c369e1
SimonaSoliman34 <simona.soliman@ivasystem.fr>
2023-03-26 22:47:52 +0200

    ignore api file

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 const apiPrefix = "/api";
 // const APIURL = process.env.REACT_APP_API_URL;
-const APIURL="https://api1qsdcawefv198.dhattendance.com"
+const APIURL="http://localhost:3000"
 console.log("API URL********",APIURL);
 const ApiUrl = APIURL + apiPrefix;
 const requestTimeout = 30000;
